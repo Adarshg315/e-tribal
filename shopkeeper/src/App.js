9325bb6b408a22335d9237c2e938c3c691a8e606
Adarshg315 <adarshg315@gmail.com>
2020-11-04 11:36:26 +0530

    updated app.js

--- shopkeeper/src/App.js
+++ shopkeeper/src/App.js
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CreateProduct from './components/create-product.component';
