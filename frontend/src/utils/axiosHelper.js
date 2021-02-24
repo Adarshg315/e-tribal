573c16eb76bdecf99737aa20185f6e128f4142ef
Adarshg315 <adarshg315@gmail.com>
2021-02-24 20:30:03 +0530

    axoios helper updated

--- frontend/src/utils/axiosHelper.js
+++ frontend/src/utils/axiosHelper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL: "https://determined-meitner-b0c027.netlify.app/",
+	baseURL: "http://localhost:5050/",
 	responseType: "json",
 });
